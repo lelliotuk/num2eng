3b01dbb4cc637c8f4ff823ef1fb8f44482c89420
Elliot <73572481+lelliotuk@users.noreply.github.com>
2021-08-16 17:52:51 +0100

    misc

--- num2eng.py
+++ num2eng.py
@@ -78,10 +78,10 @@ def int2eng(num):
 
 
 def eng2int(num):
-    num = num.replace(",", " ")
-    num = num.replace("-", " ")
-    num = num.lower()
-    num = num.split(" ")
+    num = num.replace(",", " ") \
+             .replace("-", " ") \
+             .lower() \
+             .split(" ")
 
     out = 0
     hund = 0
