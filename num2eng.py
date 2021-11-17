2ddaa6d53d56174bc1d015c46d922386d937481b
Elliot <73572481+lelliotuk@users.noreply.github.com>
2021-11-17 00:35:20 +0000

    extra stuff

--- num2eng.py
+++ num2eng.py
@@ -1,3 +1,5 @@
+import re
+
 DIGITS = ("zero", "one", "two", "three", "four", "five", "six", "seven",
           "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
           "fifteen", "sixteen", "seventeen", "eighteen", "nineteen")
@@ -19,6 +21,27 @@ POW3 = ("thousand", "million", "billion", "trillion", "quadrillion",
         "novemtrigintillion")
 
 MAX_INT = 10**123 - 1
+
+
+ORDINAL = {1: "first",
+           2: "second",
+           3: "third",
+           5: "fifth",
+           8: "eighth",
+           9: "ninth",
+           12: "twelfth",
+           20: "twentieth",
+           30: "thirtieth",
+           40: "fortieth",
+           50: "fiftieth",
+           60: "sixtieth",
+           70: "seventieth",
+           80: "eightieth",
+           90: "ninetieth"}
+
+ORDINAL.update(list(map(reversed, ORDINAL.items())))
+
+TH = ("th", "st", "nd", "rd")
 
 
 def hund(num, add_and=False):
@@ -51,18 +74,18 @@ def hund(num, add_and=False):
         return word
 
 
-def int2eng(num):
+def int2eng(num, ordinal=False):
     if num < 0 or num > MAX_INT:
         raise ValueError("Integer must meet: 0 >= integer < 10^123")
 
     word = None
     if num < 1000:
-        return hund(num)
+        word = hund(num)
     elif num:
         if num % 1000:
             word = hund(num % 1000, add_and=True)
     else:
-        return hund(num)
+        word = hund(num)
 
     for i, p3 in enumerate(POW3):
         pw = 1000 ** (i + 1)
@@ -74,14 +97,19 @@ def int2eng(num):
         else:
             word = f"{hund(h)} {POW3[i]}"
 
-    return word
+    if ordinal:
+        return card2ord(word)
+    else:
+        return word
 
 
 def eng2int(num):
+    num = ord2card(num)
     num = num.replace(",", " ") \
              .replace("-", " ") \
              .lower() \
              .split(" ")
+
 
     out = 0
     hund = 0
@@ -103,3 +131,35 @@ def eng2int(num):
 
     out += hund
     return out
+
+
+last_re = re.compile("[^-\s][a-zA-Z]+$")
+
+
+def card2ord(num):  # not ideal
+    last = last_re.search(num).group()
+    num_int = eng2int(last)
+    last = ORDINAL.get(num_int) or last + TH[0]
+    out = last_re.sub(last, num)
+    return out
+
+
+def ord2card(num):
+    num = num.lower()
+    last = last_re.search(num).group()
+    ord_int = ORDINAL.get(last)
+    last = int2eng(ord_int) if ord_int else last.rstrip(TH[0])
+    out = last_re.sub(last, num)
+    return out
+    
+
+def nth(num):
+    num_abs = abs(int(num))
+    digit = num_abs % 10
+    tens = num_abs % 100 // 10
+    num = str(num)
+    if digit >= 1 and digit <= 3 and tens != 1:
+        return num + TH[digit]
+    else:
+        return num + TH[0]
+
